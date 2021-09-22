da6fe10c5183dfe583d4c78e7ea99590489cc6d4
atroncoso2 <atroncoso2@uc.cl>
2021-09-22 18:40:49 -0300

    initial tests, includiing working heaviside

--- t1/test2.asm
+++ t1/test2.asm
@@ -1,7 +1,7 @@
 .globl  start
 .data
     # --- TERREMOTO ---
-    I: .word H, P, G 	# porcentajes de cada uno de los ingredientes, siempre suman 100
+    I: .word 5, 5, 90 	# porcentajes de cada uno de los ingredientes, siempre suman 100
     # No modificar
     Wa: .word 7, 3, 2 	# pesos w_a para el primer perceptron
     Wb: .word 4, 2, 8 	# pesos w_b para el segundo perceptron
@@ -11,4 +11,9 @@
 .text
     start:
         # aca va su codigo  :3
+        li a0, 2 # a0 = 2 
+        li a7, 4 # a7 = 4 
+        add t0, a0, a7 # t0 = a0 + a7
         
+        la t0, I
+        lw a2, I
